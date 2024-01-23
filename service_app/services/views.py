6c5a1f356b38dd49b92ed1d0985c23d7053104ac
jobs04@list.ru <jobs04@list.ru>
2024-01-23 15:04:10 +0300

    using redis cache

--- service_app/services/views.py
+++ service_app/services/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db.models import Prefetch, F, Sum
+from django.core.cache import cache
 from rest_framework import response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -20,11 +22,19 @@ class SubsciptionView(ReadOnlyModelViewSet):
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-
         response = super().list(request, *args, **kwargs)
 
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+        if price_cache:
+            total_price = price_cache
+            print('!!!!! get from cache.')
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60*60)
+            print('!!!!! get from db.')
+
         response_data = {'result': response.data}
-        response_data['total_ammount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response_data['total_ammount'] = total_price
         response.data = response_data
 
         return response
